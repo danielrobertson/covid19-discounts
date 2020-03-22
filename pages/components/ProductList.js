fa7349b6974293ee9d74acd2308cc339787c8b6e
Daniel Robertson <danielrobertson733@gmail.com>
2020-03-22 04:30:51 -0500

    product card prototype

---
+++ pages/components/ProductList.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import {
+  Box,
+  PseudoBox,
+  Stack,
+  Image,
+  Text,
+  Badge,
+  Link,
+  Flex
+} from "@chakra-ui/core";
+import Product from "./Product";
+
+function ProductList({ products }) {
+  const product = {
+    url:
+      "https://www.theverge.com/2020/3/16/21181915/synth-apps-free-moog-korg-kaossilator-minimoog-model-d-coronavirus",
+    image:
+      "https://cdn.vox-cdn.com/thumbor/qddoUUO-My0VeIVGOy9dUdUVYg4=/0x0:640x365/920x613/filters:focal(269x132:371x234):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/66508573/dims.0.jpg",
+    description:
+      "Moog and Korg are offering synth apps for free while we’re all stuck at home",
+    tags: ["music", "mobile app"]
+  };
+
+  products.push(product);
+  console.log(products);
+  const productList = products.map(p => <Product key={p.url} product={p} />);
+  return <Box>{productList}</Box>;
+}
+
+export default ProductList;
--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,16 @@
 import React from "react";
 
-import {
-  Box,
-  ThemeProvider,
-  ColorModeProvider,
-  LightMode,
-  CSSReset,
-  Stack,
-  Image,
-  Text,
-  Link,
-  Divider,
-  Button,
-  Icon,
-  Flex,
-  Avatar,
-  PseudoBox,
-  Heading
-} from "@chakra-ui/core";
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import Header from "./components/header";
+import ProductList from "./components/ProductList";
 
 function Index() {
+  const products = [];
   return (
     <ThemeProvider>
       <CSSReset />
-      <header>
-        <Box
-          h="600px"
-          w="100%"
-          backgroundImage="url('https://i.imgur.com/FJVb8vf.png')"
-          backgroundRepeat="no-repeat"
-          backgroundSize="cover"
-        >
-          <Flex justify="center">
-            <Stack mt="190px">
-              <Flex justify="center">
-                <Heading
-                  mb={4}
-                  color="white"
-                  fontSize="6xl"
-                  letterSpacing={0.5}
-                >
-                  Discounted Products
-                </Heading>
-              </Flex>
-              <Flex justify="center">
-                <Text
-                  fontWeight="semibold"
-                  color="white"
-                  fontSize="2xl"
-                  letterSpacing={0.5}
-                >
-                  In the era of COVID-19 and social distancing recommendations
-                </Text>
-              </Flex>
-              <Flex justify="center"></Flex>
-            </Stack>
-          </Flex>
-        </Box>
-      </header>
+      <Header />
+      <ProductList products={products} />
     </ThemeProvider>
   );
 }
