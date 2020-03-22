f70243dcb3889687a0ae0cfcc3d6aa11fdd9ef72
Daniel Robertson <danielrobertson733@gmail.com>
2020-03-22 04:30:51 -0500

    product card prototype

---
+++ pages/components/ProductList.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+import {
+  Box,
+  PseudoBox,
+  Stack,
+  Image,
+  Text,
+  Badge,
+  Link,
+  Flex
+} from "@chakra-ui/core";
+
+function ProductList({ products }) {
+  const product = {
+    url:
+      "https://www.theverge.com/2020/3/16/21181915/synth-apps-free-moog-korg-kaossilator-minimoog-model-d-coronavirus",
+    image:
+      "https://cdn.vox-cdn.com/thumbor/qddoUUO-My0VeIVGOy9dUdUVYg4=/0x0:640x365/920x613/filters:focal(269x132:371x234):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/66508573/dims.0.jpg",
+    description:
+      "Moog and Korg are offering synth apps for free while we’re all stuck at home",
+    tags: ["music", "mobile app"]
+  };
+  console.log(products);
+  return (
+    <PseudoBox
+      _hover={{
+        boxShadow:
+          "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)"
+      }}
+      m="40px"
+      maxW="md"
+      shadow="sm"
+      borderWidth="1px"
+      rounded="lg"
+      overflow="hidden"
+    >
+      <Link href={product.url} isExternal>
+        <Stack>
+          <Image rounded="md" src={product.image} />
+          <Stack p="15px">
+            <Text fontWeight="semibold" fontSize="2xl">
+              {product.description}
+            </Text>
+            <Text color="gray.600" fontWeight="bold" fontSize="sm">
+              {product.tags.map(t => (
+                <Badge key={t} rounded="lg" mr={3}>
+                  {t}
+                </Badge>
+              ))}
+            </Text>
+          </Stack>
+        </Stack>
+      </Link>
+    </PseudoBox>
+  );
+}
+
+export default ProductList;
