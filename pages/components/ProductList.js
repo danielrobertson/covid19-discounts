b23ca3faaddeb578b6267612f80065e142992ec4
Daniel Robertson <danielrobertson733@gmail.com>
2020-05-02 21:45:50 -0500

    search and filter

--- pages/components/ProductList.js
+++ pages/components/ProductList.js
@@ -1,28 +1,67 @@
 import React from "react";
 
-import { Box, Grid } from "@chakra-ui/core";
+import {
+  Grid,
+  Input,
+  Stack,
+  Flex,
+  InputRightElement,
+  InputGroup,
+  IconButton,
+} from "@chakra-ui/core";
 import Product from "./Product";
 
 function ProductList({ products }) {
-  console.log(products);
-  const productList = products.map((p) => (
-    <Box>
-      <Product key={p.url} product={p} />
-    </Box>
-  ));
+  const [searchValue, setSearchValue] = React.useState("");
+  const handleSearchInput = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const productList = products
+    .filter((p) => {
+      const toLwr = (str) => str.toLowerCase();
+      const tags = p.tags.join();
+      if (searchValue === "") return p;
+      else if (
+        toLwr(p.description).includes(toLwr(searchValue)) ||
+        toLwr(tags).includes(toLwr(searchValue))
+      ) {
+        return p;
+      }
+    })
+    .map((p) => <Product key={p.url} product={p} />);
+
   return (
-    <Grid
-      mx={[0, 20, 20, 20]}
-      templateColumns={[
-        "repeat(auto-fill, minmax(370px, 1fr))",
-        "repeat(auto-fill, minmax(490px, 1fr))",
-        "repeat(auto-fill, minmax(490px, 1fr))",
-        "repeat(auto-fill, minmax(490px, 1fr))",
-      ]}
-      gap={6}
-    >
-      {productList}
-    </Grid>
+    <Stack>
+      <Flex justifyContent="flex-end" paddingTop={3} paddingRight="150px">
+        <InputGroup>
+          <Input
+            value={searchValue}
+            onChange={handleSearchInput}
+            placeholder="Search..."
+            size="lg"
+            w="300px"
+            rounded="18px"
+          />
+          <InputRightElement>
+            <IconButton aria-label="Search" icon="search" variant="ghost" />
+          </InputRightElement>
+        </InputGroup>
+      </Flex>
+
+      <Grid
+        mx={[0, 20, 20, 20]}
+        templateColumns={[
+          "repeat(auto-fill, minmax(370px, 1fr))",
+          "repeat(auto-fill, minmax(490px, 1fr))",
+          "repeat(auto-fill, minmax(490px, 1fr))",
+          "repeat(auto-fill, minmax(490px, 1fr))",
+        ]}
+        gap={6}
+      >
+        {productList}
+      </Grid>
+    </Stack>
   );
 }
 
